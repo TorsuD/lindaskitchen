37da7cf14452c6c943cf0d061db3a65520d96517
TorsuD <torsuderrick2004@gmail.com>
2025-09-26 19:04:50 +0000

    fix on theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { APP_DESCRIPTION, APP_NAME } from "@/lib/constant";
-import { ThemeProvider } from "@/lib/provider";
+// import { ThemeProvider } from "@/lib/provider";
 
 const font = Quicksand({
   weight: "400",
@@ -26,17 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${font.className} antialiased`}>
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
-          <Header />
-          {/* <div className="md:pb-36 pb-10 flex-1 max-w-7xl lg:mx-auto md:px-10 px-3 w-full"> */}
-          {children}
-          {/* </div> */}
-        </ThemeProvider>
+        > */}
+        <Header />
+        {/* <div className="md:pb-36 pb-10 flex-1 max-w-7xl lg:mx-auto md:px-10 px-3 w-full"> */}
+        {children}
+        {/* </div> */}
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
